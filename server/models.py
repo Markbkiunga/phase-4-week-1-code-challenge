133fc6cbaff36b7a2b0b1cdc99463dcc1d2d8a14
Mark Brian <markbkiunga2017@gmail.com>
2024-10-06 15:22:27 +0300

    Initial commit

---
+++ server/models.py
@@ -0,0 +1,77 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = "heroes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+
+    # add relationship
+    hero_powers = db.relationship(
+        "HeroPower", back_populates="hero", cascade="all, delete-orphan"
+    )
+    powers = association_proxy(
+        "hero_powers", "power", creator=lambda power_obj: HeroPower(power=power_obj)
+    )
+    # add serialization rules
+    serialize_rules = ("-")
+
+    def __repr__(self):
+        return f"<Hero {self.id}>"
+
+
+class Power(db.Model, SerializerMixin):
+    __tablename__ = "powers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+
+    # add relationship
+    hero_powers = db.relationship(
+        "HeroPower", back_populates="power", cascade="all, delete-orphan"
+    )
+    heroes = association_proxy(
+        "hero_powers", "hero", creator=lambda hero_obj: HeroPower(hero=hero_obj)
+    )
+
+    # add serialization rules
+
+    # add validation
+
+    def __repr__(self):
+        return f"<Power {self.id}>"
+
+
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__ = "hero_powers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String, nullable=False)
+
+    # add relationships
+    hero_id = db.Column(db.Integer, db.ForeignKey("heros.id"))
+    power_id = db.Column(db.Integer, db.ForeignKey("powers.id"))
+    hero = db.relationship("Hero", back_populates="hero_powers")
+    power = db.relationship("Power", back_populates="hero_powers")
+
+    # add serialization rules
+
+    # add validation
+
+    def __repr__(self):
+        return f"<HeroPower {self.id}>"
